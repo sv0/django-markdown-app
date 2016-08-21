15c9f57830b08f402840eaf39b5f19fecc6214c1
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2016-08-21 16:03:31 +0200

    change project name to 'django-markdown-utils'. change version to '0.8.5'

--- django_markdown/__init__.py
+++ django_markdown/__init__.py
@@ -1,6 +1,6 @@
 """ Django-Markdown supports markdown in Django. """
 
-__version__ = "0.8.4"
-__project__ = "django-markdown"
+__version__ = "0.8.5"
+__project__ = "django-markdown-utils"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "GNU LGPL"
