4a53b1330ee2d691f0fc0a63a465ba367723a41b
Rob Speed <speed.rob@gmail.com>
2017-10-17 18:45:28 -0400

    FIX: Multiple editors with different selectors/configs
    * Changes config JSON element from using an id to a class.
    * Adds the ability to load multiple JSON config elements.
    * Moves the `editor.js` inclusion from the template to the widget.

--- django_markdown/static/django_markdown/editor.js
+++ django_markdown/static/django_markdown/editor.js
@@ -1,10 +1,11 @@
-(function($) {
-    var config = JSON.parse($("#markItUpEditorConfig").text());
-    $(document).ready(function() {
+jQuery(function ($) {
+    $('script.markItUpEditorConfig').each(function (i, config_element) {
+        var config = JSON.parse(config_element.textContent);
         $(config["selector"]).each(function (k, el) {
             var el = $(el);
-            if(!el.hasClass("markItUpEditor")) el.markItUp(
-                mySettings, config["extra_settings"]);
+            if(!el.hasClass("markItUpEditor")) {
+                el.markItUp(mySettings, config["extra_settings"]);
+            }
         });
     });
-})(jQuery);
+});
