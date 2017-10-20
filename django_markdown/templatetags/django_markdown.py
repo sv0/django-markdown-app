31231f2ee589a83020c854619592ebaaa33748c7
Dorian Amouroux <dor.amouroux@gmail.com>
2017-10-20 20:51:33 +0200

    import reverse function from `django.urls`
    RemovedInDjango20Warning: Importing from django.core.urlresolvers is deprecated in favor of django.urls

--- django_markdown/templatetags/django_markdown.py
+++ django_markdown/templatetags/django_markdown.py
@@ -2,7 +2,7 @@
 import posixpath
 
 from django import template
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from ..utils import markdown as _markdown, settings, simplejson
--- django_markdown/utils.py
+++ django_markdown/utils.py
@@ -1,7 +1,7 @@
 """ Markdown utils. """
 import markdown as markdown_module
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.template import loader
 from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
