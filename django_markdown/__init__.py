a9acf5eda4b713270c7f5d2162dae951a5b8bda4
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2017-11-09 13:52:26 +0100

    fix mistakenly bumbed version

--- django_markdown/__init__.py
+++ django_markdown/__init__.py
@@ -1,6 +1,6 @@
 """ Django-Markdown supports markdown in Django. """
 
-__version__ = "0.10.0"
+__version__ = "0.9.3"
 __project__ = "django-markdown-app"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "GNU LGPL"
