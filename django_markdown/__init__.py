6452a406096241f1a991a53eaeb9d61ca4144541
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2018-02-14 17:03:12 +0100

    bump version

--- django_markdown/__init__.py
+++ django_markdown/__init__.py
@@ -1,6 +1,6 @@
 """ Django-Markdown supports markdown in Django. """
 
-__version__ = "0.9.4"
+__version__ = "0.9.4.1"
 __project__ = "django-markdown-app"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "GNU LGPL"
