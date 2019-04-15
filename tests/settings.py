c43d435b0a67f8645a0f54598f267194b17e906c
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2019-04-15 11:56:44 +0200

    update environmnets list

--- tests/settings.py
+++ tests/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = (
     'tests',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
