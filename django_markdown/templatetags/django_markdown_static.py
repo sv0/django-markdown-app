0a9b83433c8eb2578314fcd6be0734d1ca833166
PashaQA <maileslcb26@yandex.ru>
2021-03-17 13:50:47 +0300

    fix import django.templatetags.static for Django 3

--- django_markdown/templatetags/django_markdown_static.py
+++ django_markdown/templatetags/django_markdown_static.py
@@ -3,10 +3,15 @@ from django.template import Library
 
 register = Library()
 
-if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
+try:
     from django.contrib.staticfiles.templatetags.staticfiles import static as _static  # noqa
-else:
+except Exception:
     from django.templatetags.static import static as _static
+    
+# if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
+#     from django.contrib.staticfiles.templatetags.staticfiles import static as _static  # noqa
+# else:
+#     from django.templatetags.static import static as _static
 
 
 @register.simple_tag
