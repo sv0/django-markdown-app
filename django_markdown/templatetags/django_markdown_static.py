5b36aa3f2e6eb0af72c2922effd3e997219395d6
Marco Bontempi <marco.bontempi@secretescapes.com>
2021-09-09 17:04:26 +0200

    Remove django.contrib.staticfiles templatetags incompatible in Django 3

--- django_markdown/templatetags/django_markdown_static.py
+++ django_markdown/templatetags/django_markdown_static.py
@@ -1,12 +1,8 @@
 from django.conf import settings
 from django.template import Library
+from django.templatetags.static import static as _static
 
 register = Library()
-
-if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
-    from django.contrib.staticfiles.templatetags.staticfiles import static as _static  # noqa
-else:
-    from django.templatetags.static import static as _static
 
 
 @register.simple_tag
