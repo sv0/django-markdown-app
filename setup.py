5052cf1b608518c9a8ad7094135e69ea4b09f8eb
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2023-11-24 08:28:45 +0100

    chore: add Python 3.9

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Code Generators',
         'Topic :: Text Processing :: Markup',
     ],
